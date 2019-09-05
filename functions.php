9bb6edd015b3ea62b907677196d4099e0d0200a9
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-09-05 18:15:28 +0800

    hide Post from Dashboard

--- functions.php
+++ functions.php
@@ -5,5 +5,7 @@ require_once('lib/helpers.php');
 require_once('lib/enqueue-assets.php');
 require_once('lib/sidebars.php');
 require_once('lib/menus.php');
+require_once('lib/remove_posts.php');
+
 
 
