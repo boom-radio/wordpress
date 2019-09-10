8adc1dddf7e1b1e1cbd54241b959d8c2ae57c424
Robbozinoz <Robbozinoz>
2019-09-10 15:33:24 +0800

    Updating helpers.php to include excerpt length and link control

--- archive.php
+++ archive.php
@@ -1,2 +1,9 @@
 <?php
 //Placeholder file - Main archive folder for the site
+
+//------For use when building this page-------
+// if ( is_category() || is_archive() ) {
+  //  the_excerpt();
+//} else {
+  //  the_content();
+//} //
