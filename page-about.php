2582c7e0efd17ac7c986c08500bb2fcd1015ff84
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-09-11 16:12:50 +0800

    Sorted the gradiented box background out; The code is between lines 60 and 80 of the page-about; It gives the background in an specific order. I will move this code in a function in a file inside the lib folder

--- page-about.php
+++ page-about.php
@@ -31,72 +31,147 @@ get_header(); ?>
 <!---H1 WITH WAVE -->
 <div class="grid-x grid-padding-y align-center align-middle">
     <div class="cell auto text-right">
-        <img src="assets/img/wave_left.svg" alt="wave left">
+        <img src="<?php echo get_theme_file_uri('src/assets/img/wave_left.svg'); ?>" alt="wave left">
     </div>
     <div class="cell shrink">
         <?php the_title('<h1>', '</h1>'); ?>
     </div>
     <div class="cell auto text-left">
-        <img src="assets/img/wave_right.svg" alt="wave right">
+        <img src="<?php echo get_theme_file_uri('src/assets/img/wave_right.svg'); ?>" alt="wave right">
     </div>
 </div>
 <!--End Title with Wave-->
 <div class="grid-container">
     <div class="grid-x grid-margin-x grid-margin-y align-center">
-        <div class="cell">
-            <!-- Gradiented container ????????????????????????????????????????????????????????????????????????????????????????????? -->
-            <div class="grid-container-fluid gradiented-box gradient-one-two">
-                <div class="grid-x grid-padding-x grid-padding-y align-spaced align-middle">
-                    <div class="cell medium-4">
-                        <img class="img-left box-shadowed" src="assets/img/shows/urban_jungle_lrg.jpg" alt="show image">
-                    </div>
-                    <div class="cell medium-6">
-                        <div class="grid-container-fluid">
-                            <div class="grid-x grid-padding-x grid-padding-y">
-                                <div class="cell text-justify">
+        <?php
+        $args = array(
+            'post_type' => 'about',
+            'post__in' => the_ID(),
+            'orderby' => 'post__in'
+        );
 
-                                    <?php // Get post content to extract first title.
-                                    $blocks = parse_blocks(get_the_content());
-                                    foreach ($blocks as $block) {
-                                        if ('core/first-chapter' === $block['blockName']) {
-                                            echo do_shortcode($block['innerHTML']);
-                                            break;
-                                        }
-                                    }
-                                    // Display the title.
-                                    ?>
-                                    <!--This is to attach the White boom Radio button, code in lib/helpers.php as an example of how we can resue sections of code across the site-->
-                                    <?php boom_radio_readmore_link(); ?>
+        $the_query = new WP_Query($args); ?>
 
+        <!--Start of the Loop-->
+        <?php if ($the_query->have_posts()) :
+            while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <div <?php post_class('cell'); ?>>
+                    <!-- Gradiented container ????????????????????????????????????????????????????????????????????????????????????????????? -->
+                    <div class="grid-container-fluid gradiented-box <?php
+
+                                                                            
+                                                                            $gradientedColour = array("gradient-one-two", "gradient-three-four", "gradient-five-six");
+                                                                            // $randomColour = array_rand($gradientedColour);
+                                                                            // echo $gradientedColour[$randomColour];
+                                                                            if (empty($counter)) {
+                                                                                $counter = 0;
+                                                                            } 
+
+                                                                            if ($counter == 0) {
+                                                                                echo $gradientedColour[$counter];
+                                                                                $counter++;
+                                                                            } else if ($counter == 1) {
+                                                                                echo $gradientedColour[$counter];
+                                                                                $counter++;
+                                                                            } else if ($counter == 2) {
+                                                                                echo $gradientedColour[$counter];
+                                                                                $counter = 0;
+                                                                            }
+                                                                            ?>">
+                        <div class="grid-x grid-padding-x grid-padding-y align-spaced align-middle">
+                            <div class="cell medium-4">
+                                <?php if (has_post_thumbnail()) :
+                                            the_post_thumbnail('large', array('class' => 'img-right box-shadowed'));
+                                        endif; ?>
+                            </div>
+                            <div class="cell medium-6">
+                                <div class="grid-container-fluid">
+                                    <div class="grid-x grid-padding-x grid-padding-y">
+                                        <div class="cell">
+
+                                            <?php the_title('<h2>', '</h2>'); ?>
+
+                                        </div>
+                                        <div class="cell text-justify">
+                                            <?php the_content(); ?>
+                                        </div>
+                                        <div class="cell">
+                                            <!-- <a href="https://www.northmetrotafe.wa.edu.au/courses/" class="boom-button-white float-right">Visit TAFE
+                                    </a> -->
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- Gradiented container ????????????????????????????????????????????????????????????????????????????????????????????? -->
                 </div>
+                <!-- Empty cell/s used  for spacing-->
+                <div class="cell"></div>
+                <div class="cell"></div>
+            <?php endwhile;
+                wp_reset_postdata(); ?>
+
+        <?php else : ?>
+            <p style="color: #FFF;"><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+        <!-- End of the loop.-->
+
+        <!---Title with Wave -->
+        <div class="grid-x grid-padding-y align-center align-middle">
+            <div class="cell auto text-right">
+                <img src="<?php echo get_theme_file_uri('src/assets/img/wave_left.svg'); ?>" alt="wave left">
+            </div>
+            <div class="cell shrink">
+                <h3>Terms & Conditions</h3>
+            </div>
+            <div class="cell auto text-left">
+                <img src="<?php echo get_theme_file_uri('src/assets/img/wave_right.svg'); ?>" alt="wave right">
             </div>
         </div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="grid-container-fluid gradiented-box gradient-three-four">
-            <div class="grid-x grid-padding-x grid-padding-y align-spaced align-middle">
-                <div class="cell medium-6">
-                    <div class="grid-container-fluid">
-                        <div class="grid-x grid-padding-x grid-padding-y">
-                            <div class="cell text-justify">
-
-                                
-
+        <!--END OF H3 WAVE-->
+        <div class="grid-container">
+            <div class="grid-x grid-margin-x grid-margin-y align-center">
+                <div class="cell">
+                    <!-- Gradiented container ????????????????????????????????????????????????????????????????????????????????????????????? -->
+                    <div class="grid-container-fluid gradiented-box gradient-five-six">
+                        <div class="grid-x grid-padding-x grid-padding-y align-spaced align-middle">
+                            <div class="cell medium-4">
+                                <img class="img-left box-shadowed" src="assets/img/boom_team/jay_cin_bron_lrg.jpg" alt="Show image">
+                            </div>
+                            <div class="cell medium-6">
+                                <div class="grid-container-fluid">
+                                    <div class="grid-x grid-padding-x grid-padding-y">
+                                        <div class="cell">
+                                            <h2>General Competition Terms</h2>
+                                        </div>
+                                        <div class="cell text-justify">
+                                            <p>All winners must adhere to both the contest’s and Boom Radio’s
+                                                terms
+                                                and conditions.</p>
+                                            <br>
+                                            <p>For more information click the button below</p>
+                                        </div>
+                                        <div class="cell">
+                                            <a href="terms.html" class="boom-button-white float-right">Read
+                                                More...
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <!-- Gradiented container ????????????????????????????????????????????????????????????????????????????????????????????? -->
                 </div>
-                <div class="cell medium-4">
-                    <img class="img-left box-shadowed" src="assets/img/shows/urban_jungle_lrg.jpg" alt="show image">
-                </div>
+                <!-- Empty cell/s used  for spacing-->
+                <div class="cell"></div>
+                <div class="cell"></div>
             </div>
         </div>
     </div>
 </div>
+<!-- END OF MAIN BODY ============================================================================================================================================== -->
 
 
 
