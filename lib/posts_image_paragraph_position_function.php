723bc4562af734e443474e970d53c9664a11958b
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-09-16 20:40:34 +0800

    Sorted out the image and paragraph swapping function. I have commented both of them but if you need explanations and/or help to implement it let me know. Now on the about page I have posts with alternate images on left and right and the colours go from pink to blue to orange.

--- lib/posts_image_paragraph_position_function.php
+++ lib/posts_image_paragraph_position_function.php
@@ -1,65 +1,57 @@
 <?php
+//This function is for swapping images and paragraphs left and right alternatively (img left-parag right and viceversa
 
 function posts_image_paragraph_position($postNumber)
-{
+{   
+    if (empty($postNumber)) {
+        $postNumber = 0;
+    }
+    
     if (has_post_thumbnail()) :
-        if ($postNumber % 2 == 0) {
-            $thumbnailLeft = the_post_thumbnail('large', array('class' => 'img-left box-shadowed'));
-        } else {
-            $thumbnailRight = the_post_thumbnail('large', array('class' => 'img-right box-shadowed'));
-        }
+        $featuredImgUrl = get_the_post_thumbnail_url(get_the_ID(), 'large');
+        $thumbnail = esc_url($featuredImgUrl);
     endif;
-    $postTitle = the_title('<h2>', '</h2>', 0);
+
+    $postTitle = get_the_title();
     $postContent = get_the_content();
 
     $imgLeftParagRight = '
-                        <div class="cell medium-4">' .
-
-                        $thumbnailLeft .
-
-                        '</div>
+                        <div class="cell medium-4">
+                            <img src="' . $thumbnail . '" class="img-left box-shadowed">
+                        </div>
                         <div class="cell medium-6">
                                 <div class="grid-container-fluid">
                                     <div class="grid-x grid-padding-x grid-padding-y">
-                                        <div class="cell">' .
-
-                                        $postTitle .
-
-                                        '</div>
+                                        <div class="cell">
+                                            <h2>' .
+                                            $postTitle .
+                                            '</h2>
+                                        </div>
                                         <div class="cell text-justify">' .
-
                                         $postContent .
-
                                         '</div>
                                     </div>
                                 </div>
                             </div>';
 
     $imgRightParagLeft = '
-                            <div class="cell medium-6">
-                                <div class="grid-container-fluid">
-                                    <div class="grid-x grid-padding-x grid-padding-y">
-                                        <div class="cell">' .
-
+                        <div class="cell medium-6">
+                            <div class="grid-container-fluid">
+                                <div class="grid-x grid-padding-x grid-padding-y">
+                                    <div class="cell">
+                                        <h2>' .
                                         $postTitle .
-
-                                        '</div>
-                                        <div class="cell text-justify">' .
-                                        $postContent .
-
-                                        '</div>
+                                        '</h2>
                                     </div>
+                                    <div class="cell text-justify">' .
+                                    $postContent .
+                                    '</div>
                                 </div>
-                            </div> 
-                            <div class="cell medium-4">' .
-
-                            $thumbnailLeft .
-
-                            '</div>';
-
-    if (empty($postNumber)) {
-        $postNumber = 0;
-    }
+                            </div>
+                        </div>
+                        <div class="cell medium-4">
+                            <img src="' . $thumbnail . '" class="img-left box-shadowed">
+                        </div>';
 
     if ($postNumber % 2 == 0) {
         return $imgLeftParagRight;
