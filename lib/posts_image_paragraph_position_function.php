7c6eb5cd2745e2d98d5f7c3fdc49d550c5de5870
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-09-16 20:41:21 +0800

    Sorted out the image and paragraph swapping function. I have commented both of them but if you need explanations and/or help to implement it let me know. Now on the about page I have posts with alternate images on left and right and the colours go from pink to blue to orange.

--- lib/posts_image_paragraph_position_function.php
+++ lib/posts_image_paragraph_position_function.php
@@ -3,18 +3,25 @@
 
 function posts_image_paragraph_position($postNumber)
 {   
+    // Check if the post number (basically the post order, first post on page, second post etc.) is empty; if it is, is set to 0
     if (empty($postNumber)) {
         $postNumber = 0;
     }
     
+    // Check if the post has thumbnail image (featured image)
     if (has_post_thumbnail()) :
+        // If there is a thumbnail get the URL and set it to the variable $featuredImgUrl
         $featuredImgUrl = get_the_post_thumbnail_url(get_the_ID(), 'large');
+        // Clean the URL and set the result into $thumbnail 
         $thumbnail = esc_url($featuredImgUrl);
     endif;
 
+    // Get the post title and set it into the variable $postTitle
     $postTitle = get_the_title();
+    // Get the post content and set it into the variable $postContent
     $postContent = get_the_content();
 
+    // Put the HTML for a left image and a right paragraph into the variable $imgLeftParagRight
     $imgLeftParagRight = '
                         <div class="cell medium-4">
                             <img src="' . $thumbnail . '" class="img-left box-shadowed">
@@ -34,6 +41,7 @@ function posts_image_paragraph_position($postNumber)
                                 </div>
                             </div>';
 
+    // Put the HTML for a right image and a left paragraph into the variable $imgRightParagLeft
     $imgRightParagLeft = '
                         <div class="cell medium-6">
                             <div class="grid-container-fluid">
@@ -53,9 +61,12 @@ function posts_image_paragraph_position($postNumber)
                             <img src="' . $thumbnail . '" class="img-left box-shadowed">
                         </div>';
 
+    // Check whether the post number (basically the post order, first post on page, second post etc.) is even or odd
     if ($postNumber % 2 == 0) {
+        // If the post number is even return the image on the left and the paragraph on the right
         return $imgLeftParagRight;
     } else {
+        // If the post number is odd return the image on the right and the paragraph on the left
         return $imgRightParagLeft;
     }
 }
