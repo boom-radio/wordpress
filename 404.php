2cf097aad958ec87e0fcd2392b1d282bd7bb6134
Robbozinoz <Robbozinoz>
2019-09-20 08:35:44 +0800

    404 page update

--- 404.php
+++ 404.php
@@ -1,9 +1,15 @@
 <!--basic 404 can be restyled-->
 <?php get_header(); ?>
-<div class="grid-container">
+<div class="grid-container-fluid">
     <div class="grid-x grid-margin-x grid-margin-y align-center">
-        <h3><?php esc_html_e('Nothing found here, maybe you can try to search?', 'boom_radio') ?></h3>
-        <?php get_search_form(); ?>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell">
+            <h3 class=" text-center"><?php esc_html_e('Oops sorry, we couldnt find what you are look for.', 'boom_radio') ?></h3>
+        </div>
+        <div class="cell">
+            <h3 class=" text-center"><?php esc_html_e('Maybe you can try the search bar again?', 'boom_radio') ?></h3>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
