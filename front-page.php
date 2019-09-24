f8489cfeb42835f60c47b8a8335ce4261e4c1d09
Robbozinoz <Robbozinoz>
2019-09-24 17:04:03 +0800

    Updated the posts in front page to have one each of news, music and events. Updated the competition post

--- front-page.php
+++ front-page.php
@@ -27,7 +27,7 @@ get_header(); ?>
     <!--Start of card section for maximum of three Artist posts-->
     <div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-3">
 
-        <!--wp query arg set for card section-->
+        <!-----------------------------Start Last post News---------------------------------->
         <?php
         $args = array(
             //
@@ -53,7 +53,7 @@ get_header(); ?>
         <?php if ($the_query->have_posts()) :
             //Set varibale for the loop to control amount of posts
             $i = 1;
-            while ($the_query->have_posts() && $i < 4) : $the_query->the_post(); ?>
+            while ($the_query->have_posts() && $i < 2) : $the_query->the_post(); ?>
 
                 <div <?php post_class('cell'); ?>>
                     <div class="card">
@@ -101,8 +101,160 @@ get_header(); ?>
         <?php else : ?>
             <p style="color: #FFF;"><?php _e('Sorry, no posts matched your criteria.'); ?></p>
         <?php endif; ?>
-        <?php the_posts_pagination(); ?>
+        <!---------------------End Last post News--------------------------->
 
+        <!---------------------Last post Music--------------------------->
+        <!--wp query arg set for card section-->
+        <?php
+        $args = array(
+            //
+            'post_type' => 'music_post',
+            //Set maximum to three
+            'post__in'            => get_option('sticky_posts'),
+            'ignore_sticky_posts' => 1,
+            'orderby'   => array(
+                'date' => 'DESC',
+            ),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'category_music',
+                    'field'    => 'slug',
+                    //Album term id number in db
+                    'terms' =>  'album'
+                )
+            )
+        );
+        $the_query = new WP_Query($args); ?>
+
+        <!--Start of the Loop-->
+        <?php if ($the_query->have_posts()) :
+            //Set varibale for the loop to control amount of posts
+            $i = 1;
+            while ($the_query->have_posts() && $i < 2) : $the_query->the_post(); ?>
+
+                <div <?php post_class('cell'); ?>>
+                    <div class="card">
+
+                        <?php if (has_post_thumbnail()) { ?>
+                            <img class="single-card" src="<?php the_post_thumbnail_url(); ?>" />
+                        <?php } else {
+                                    echo '<img src="' . get_bloginfo("template_url") . '/src/assets/img/img-default.png" />';
+                                }
+                                ?>
+                        <div class="card-section">
+                            <div class="bio">
+                                <h5 class="section-title">
+                                    <?php the_title() ?>
+                                </h5>
+                                <?php the_excerpt(); ?>
+                            </div>
+                            <div class="cell">
+                                <div class="animation-container">
+                                    <div class="sound-container">
+                                        <div class="rect-2"></div>
+                                        <div class="rect-3"></div>
+                                        <div class="rect-4"></div>
+                                        <div class="rect-5"></div>
+                                        <div class="rect-6"></div>
+                                        <div class="rect-5"></div>
+                                        <div class="rect-4"></div>
+                                        <div class="rect-3"></div>
+                                        <div class="rect-2"></div>
+                                        <div class="rect-1"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--This is to attach the Orange boom button button, code in lib/helpers.php as an example of how we can resue sections of code across the site-->
+                            <?php boom_radio_card_link(); ?>
+                        </div>
+                    </div>
+                </div>
+            <?php $i++;
+                endwhile;
+                //Ensure global wp variable are set to default after custom query
+                wp_reset_postdata(); ?>
+
+        <?php else : ?>
+            <p style="color: #FFF;"><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+        <!------------------End Last post Music--------------------------->
+
+        <!--------------------- Start Last post Events--------------------------->
+        <!--wp query arg set for card section-->
+        <?php
+        $args = array(
+            //
+            'post_type' => 'music_post',
+            //Set maximum to three
+            'post__in'            => get_option('sticky_posts'),
+            'ignore_sticky_posts' => 1,
+            'orderby'   => array(
+                'date' => 'DESC',
+            ),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'category_music',
+                    'field'    => 'slug',
+                    //Album term id number in db
+                    'terms' =>  'event'
+                )
+            )
+        );
+        $the_query = new WP_Query($args); ?>
+
+        <!--Start of the Loop-->
+        <?php if ($the_query->have_posts()) :
+            //Set varibale for the loop to control amount of posts
+            $i = 1;
+            while ($the_query->have_posts() && $i < 2) : $the_query->the_post(); ?>
+
+                <div <?php post_class('cell'); ?>>
+                    <div class="card">
+                        <?php if (has_post_thumbnail()) { ?>
+                            <img class="single-card" src="<?php the_post_thumbnail_url(); ?>" />
+                        <?php } else {
+                                    echo '<img src="' . get_bloginfo("template_url") . '/src/assets/img/img-default.png" />';
+                                }
+                                ?>
+                        <div class="card-section">
+                            <div class="bio">
+                                <h5 class="section-title">
+                                    <?php the_title() ?>
+                                </h5>
+                                <?php the_excerpt(); ?>
+                            </div>
+                            <div class="cell">
+                                <div class="animation-container">
+                                    <div class="sound-container">
+                                        <div class="rect-2"></div>
+                                        <div class="rect-3"></div>
+                                        <div class="rect-4"></div>
+                                        <div class="rect-5"></div>
+                                        <div class="rect-6"></div>
+                                        <div class="rect-5"></div>
+                                        <div class="rect-4"></div>
+                                        <div class="rect-3"></div>
+                                        <div class="rect-2"></div>
+                                        <div class="rect-1"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--This is to attach the Orange boom button button, code in lib/helpers.php as an example of how we can resue sections of code across the site-->
+                            <?php boom_radio_card_link(); ?>
+                        </div>
+                    </div>
+                </div>
+            <?php $i++;
+                endwhile;
+                //Ensure global wp variable are set to default after custom query
+                wp_reset_postdata(); ?>
+
+        <?php else : ?>
+            <p style="color: #FFF;"><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+        <!------------------End Last post Events--------------------------->
     </div>
 </article>
 <div class="cell"></div>
@@ -210,7 +362,7 @@ get_header(); ?>
                 <img src="<?php echo get_theme_file_uri('src/assets/img/wave_left.svg'); ?>" alt="wave left">
             </div>
             <div class="cell shrink">
-                <h3>Social</h3>
+                <h3>Competition</h3>
             </div>
             <div class="cell auto text-left">
                 <img src="<?php echo get_theme_file_uri('src/assets/img/wave_right.svg'); ?>" alt="wave right">
--- lib/thumbnails_display_and_link.php
+++ lib/thumbnails_display_and_link.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 //function to set featured thumbnail image to lead to the original post
-function wcs_auto_link_post_thumbnails( $html, $post_id, $post_image_id ) {
-    $html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_the_title( $post_id ) ) . '">' . $html . '</a>';
+function wcs_auto_link_post_thumbnails($html, $post_id, $post_image_id)
+{
+    $html = '<a href="' . get_permalink($post_id) . '" title="' . esc_attr(get_the_title($post_id)) . '">' . $html . '</a>';
     return $html;
-    }
-add_filter( 'post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 10, 3 );
+}
+add_filter('post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 10, 3);
