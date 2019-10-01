223bdba6dfe70dcc1c8cec27c0e6a0ff789d1771
Robbozinoz <Robbozinoz>
2019-10-01 17:45:57 +0800

    Change all slick carousel imports to header cdn ready for distribution

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -12,9 +12,11 @@ function _themename_assets()
     //REPLACED  with import in app.scss ---- Slick carousel stylesheets 
     //wp_enqueue_style('boom_radio_slick', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.css', array(), '1.0.0', 'all');
     //wp_enqueue_style('boom_radio-slick_theme', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick-theme.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('boom_radio_slick', '//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css', false, '1.8.1', 'all');
+    wp_enqueue_style('boom_radio-slick_theme', '//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css', false, '1.8.1', 'all');
 
     //Foundation CDN files
-    wp_enqueue_style('foundation_min_stylesheet', '//cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/css/foundation.min.css', false, '1.0.0', 'all');
+    wp_enqueue_style('foundation_min_stylesheet', '//cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/css/foundation.min.css', false, '6.5.3', 'all');
 
     //Main theme stylesheet
     wp_enqueue_style('boom_radio_stylesheet', get_template_directory_uri() . '/style.css', false, '1.0.0', 'all');
@@ -32,12 +34,12 @@ function _themename_assets()
     // CDN hosted jQuery migrate for compatibility with jQuery 3.x
     wp_register_script('jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array('jquery'), '3.0.1', false);
 
-
     //Foundation scripts CDN
-    wp_enqueue_script('foundation_min_scripts', '//cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/js/foundation.min.js', array('jquery'), null, true);
+    wp_enqueue_script('foundation_min_scripts', '//cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/js/foundation.min.js', array('jquery'), '6.5.3', true);
 
     //Enqueues the scripts for the listen slick carousel
-    wp_enqueue_script('boom_radio_slick_scripts', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.js', array('jquery'), '1.0.0', true);
+    //wp_enqueue_script('boom_radio_slick_scripts', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('boom_radio_slick_scripts', '//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true);
 
     //Enqueue the theme's purpose built js file - REVIEW PATH FOR DISTRIBUTION
     wp_enqueue_script('boom_radio_scripts', get_template_directory_uri() . '/src/assets/js/app.js', array('jquery'), '1.0.0', true);
