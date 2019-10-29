c47a06494b73d1795b16c266d64660c7fd1af7d6
Robbozinoz <Robbozinoz>
2019-10-29 16:05:38 +0800

    Fix for thumbnail link function

--- lib/thumbnails_display_and_link.php
+++ lib/thumbnails_display_and_link.php
@@ -1,30 +1,8 @@
 <?php
 //function to set featured thumbnail image to lead to the original post
-//function wcs_auto_link_post_thumbnails($html, $post_id, $post_image_id)
-//{
-//Addition of sprintf function to format the string for 'page-listen.php' conditional loop workshould the function be kept
-//  $html = sprintf('<a href="' . get_permalink($post_id) . '" title="' . esc_attr(get_the_title($post_id)) . '">' . $html . '</a>');
-//return $html;
-//}
-//add_filter('post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 10, 3);
-
-
-//function to set featured thumbnail image to lead to the original post
-function image_link_control()
+function wcs_auto_link_post_thumbnails($html, $post_id, $post_image_id)
 {
-    if (is_page('listen') && is_post_type('presenter')) {
-        add_filter('post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 10, 3);
-        //add_filter('post_thumbnail_html', 'wcs_auto_link_post_thumbnails');
-        function wcs_auto_link_post_thumbnails($html, $post_id)
-        {
-
-            $html = '</h5>Hello from function<h5> <a href="' . get_permalink($post_id) . '" title="' . esc_attr__(get_the_title($post_id)) . '">' . $html . '</a>';
-            return $html;
-            //function to set featured thumbnail image to lead to the original post
-            //$html = '<a href="' . get_permalink($post_id) . '" title="' . esc_attr(get_the_title($post_id)) . '">' . $html . '</a>';
-            //return $html;
-        }
-    }
+    $html = '<a href="' . get_permalink($post_id) . '" title="' . esc_attr(get_the_title($post_id)) . '">' . $html . '</a>';
+    return $html;
 }
-
-add_action('image_link_filter', 'image_link_control');
+add_filter('post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 99, 3);
--- single.php
+++ single.php
@@ -29,7 +29,7 @@ get_header(); ?>
                                 <?php if (has_post_thumbnail()) {
                                         the_post_thumbnail('card_single');
                                     } else {
-                                        echo '<img src="' . get_bloginfo("template_url") . '/src/assets/img/img-default.png"/>';
+                                        echo '<img style="width: 100%;" src="' . get_bloginfo("template_url") . '/src/assets/img/img-default.png"/>';
                                     }
                                     ?>
                                 <div class="card-section">
