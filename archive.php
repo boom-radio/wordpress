a28b048a8247da50dc603dae0613d092e8668b7c
Robbozinoz <Robbozinoz>
2019-10-30 15:11:36 +0800

    Creation of page-sponsor template plus creation of posts

--- archive.php
+++ archive.php
@@ -25,7 +25,7 @@
                                         <div class="grid-x grid-padding-x">
                                             <h2>
                                                 <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_title() ?></a>
-                                            </h2>git push origin master
+                                            </h2>
                                             <!--OR use the_content for full post, this can be split into template parts at the end;-->
                                             <?php the_excerpt(); ?>
                                         </div>
