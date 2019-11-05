d2d3ff5d196f6fbf3b8110ccb62a7258ec34c7ab
MamzelNyf <fanny.lefert@hotmail.fr>
2019-11-05 17:50:15 +0800

    update styling comments and contact parts, update tabs closing on listen, refine mobile player :0,  deleted a lost ?> :)

--- page-listen.php
+++ page-listen.php
@@ -19,7 +19,7 @@ get_header(); ?>
 <div class="grid-container">
     <div class="cell ">
         <div class="md-tabs">
-            <ul class="tabs" data-responsive-accordion-tabs="tabs small-accordion medium-tabs large-tabs" id="collapsing-tabs" data-allow-all-closed="true" data-multi-expand="true" data-deep-link="true">
+            <ul class="tabs" data-responsive-accordion-tabs="tabs small-accordion medium-tabs large-tabs" id="collapsing-tabs" data-allow-all-closed="true" data-multi-expand="false" data-deep-link="true">
                 <li class="tabs-title is-active"></span><a href="#big-breakfast"><i class="fas fa-microphone-alt"></i> BOOM's Big
                         Breakfast</a></li>
                 <li class="tabs-title"></span><a href="#drive-home"><i class="fas fa-microphone-alt"></i> The Drive Home</a>
