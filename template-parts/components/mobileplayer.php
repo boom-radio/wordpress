b8d5f15b4d535248885541a4be585d95ef4d5a59
MamzelNyf <fanny.lefert@hotmail.fr>
2019-11-05 20:12:11 +0800

    update navbar to shrink under media player, contact element width, gotothetop over the presenter

--- template-parts/components/mobileplayer.php
+++ template-parts/components/mobileplayer.php
@@ -1,6 +1,6 @@
 <!-- Showing the music player in the body -->
 <div class="grid-container show-for-small hide-for-large">
-    <div class="grid-x align-middle">
+    <div class="grid-x align-middle align-center">
         <div class="cell medium-3"></div>
         <div class="cell medium-6 gradiented-box gradient-five-six">
             <iframe class="boomPlayer" src="https://tunein.com/embed/player/s195836/"></iframe>
