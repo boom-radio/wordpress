b7dd77910215c4cb859b4275155fb964adc9a6ff
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-11-06 11:47:28 +0800

    Fixed the animation on the logo.

--- template-parts/navigation/main_navbar.php
+++ template-parts/navigation/main_navbar.php
@@ -11,7 +11,7 @@
                                 <div class="grid-x grid-padding-y align-justify align-middle">
                                     <div class="cell small-1"></div>
                                     <div class="cell small-10 text-center">
-                                        <a href="<?php echo get_option("siteurl"); ?>"><img id="logo" src="<?php echo get_theme_file_uri('src/assets/img/boom_logo_orange.svg'); ?>" alt="Boom Radio logo"></a>
+                                        <a href="<?php echo get_option("siteurl"); ?>"><img src="<?php echo get_theme_file_uri('src/assets/img/boom_logo_orange.svg'); ?>" alt="Boom Radio logo"></a>
                                     </div>
                                     <!-- Empty cell/s used  for spacing  THIS CELL IS NEEDED TO CENTER THE logo -->
                                     <div class="cell small-1">
