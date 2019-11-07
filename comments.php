d8c7df4fd85a931e92cd3073b81a5861ad298a47
Robbozinoz <Robbozinoz>
2019-11-07 16:14:37 +0800

    Reinstallation of comment callback additional styling

--- comments.php
+++ comments.php
@@ -1,53 +1,48 @@
-<?php if (post_password_required()) { ?>
-    <?php return; ?>
-<?php } ?>
+<?php
 
-<div class="grid-container">
-    <div class="grid-x grid-margin-x grid-margin-y">
-        <div class="cell">
-            <div id="comments" class="c-comments">
-                <?php if (have_comments()) { ?>
-                    <h3 class="c-comments__title">
+if (post_password_required()) {
+    return;
+}
+?>
 
-                        <?php
-                            /* translators: 1 is number of comments  and 2 is post title*/
-                            printf(
-                                esc_html__(_n(
-                                    '%1$s Reply to "%2$s"',
-                                    '%1$s Replies to "%2$s"',
-                                    get_comments_number(),
-                                    'boom_radio'
-                                )),
-                                number_format_i18n(get_comments_number()),
-                                get_the_title()
-                            )
-                            ?>
-                    </h3>
+<div id="comments" class="c-comments">
+    <?php if (have_comments()) { ?>
+        <h2 class="c-comments__title">
 
-                    <!--List the comment fo the post-->
-                    <ul class="c-comments__list">
-                        <?php wp_list_comments(array(
-                                'short_ping' => true,
-                                'avatar_size' => 50,
-                                'reply_text' => 'Reply',
-                                //Callback to function to create comment markup
-                                'callback' => 'boom_radio_comment_callback'
-                            ));
-                            ?>
-                    </ul>
+            <?php
+                /* translators: 1 is number of comments  and 2 is post title*/
+                printf(
+                    esc_html__(_n(
+                        '%1$s Reply to "%2$s"',
+                        '%1$s Replies to "%2$s"',
+                        get_comments_number(),
+                        'boom_radio'
+                    )),
+                    number_format_i18n(get_comments_number()),
+                    get_the_title()
+                )
+                ?>
+        </h2>
 
-                    <?php the_comments_pagination() ?>
-                <?php } ?>
-                <!--Send message if there are comments but comments are closed-->
-                <?php if (!comments_open() && get_comments_number()) { ?>
-                    <p class="c-comments_closed"><?php esc_html_e('Comments are closed for this post', '_themename') ?></p>
-                <?php } ?>
-                <!--Display the fomr outside the loop-->
-                <div class="cell auto">
-                    <?php comment_form() ?>
-                </div>
-            </div>
+        <!--List the comment fo the post-->
+        <div class="c-comments__list">
+            <?php wp_list_comments(array(
+                    'short_ping' => true,
+                    'avatar_size' => 50,
+                    'reply_text' => 'Reply',
+                    //Callback to function to create comment markup
+                    'callback' => 'boom_radio_comment_callback'
+                ));
+                ?>
         </div>
-    </div>
+
+        <?php the_comments_pagination() ?>
+    <?php } ?>
+    <!--Send message if there are comments but comments are closed-->
+    <?php if (!comments_open() && get_comments_number()) { ?>
+        <p class="c-comments_closed"><?php esc_html_e('Comments are closed for this post', 'boom_radio') ?></p>
+    <?php } ?>
+
+    <!--Display the fomr outside the loop-->
+    <?php comment_form() ?>
 </div>
-<!--</div>-->
